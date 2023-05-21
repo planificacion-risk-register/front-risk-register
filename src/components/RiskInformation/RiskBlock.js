8f82467f4433b978c8c3953bcff97f931a8fbde7
Shesly <shesly.obando@ucr.ac.cr>
2023-05-21 16:18:46 -0600

    aplicando css en el bloque de riesgos

--- src/components/RiskInformation/RiskBlock.js
+++ src/components/RiskInformation/RiskBlock.js
@@ -1,124 +1,124 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Form from "react-bootstrap/Form";
-import "./block.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import "./block.css";
 
 const RiskBlock = ({ riskList, setDisplayChange }) => {
   return (
     <div className="container">
       <div className="table-wrapper-scroll-y">
         {riskList.map((data, index) => (
-          <Form style={{ maxWidth: "1000px", margin: "auto" }}>
-            <div className="row">
-              <div className="col">
-                <label class="form-label">Id Risk</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.id_risk}
-                />
+          <div className="risk-block">
+            <Form className="risk-form">
+              <div className="row">
+                <div className="col">
+                  <label className="form-label">Id Risk</label>
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.id_risk}
+                  />
+                </div>
+                <div className="col">
+                  <label className="form-label">Impact</label>
+                  <input
+                    type="text"
+                    readOnly
+                    className={`form-control-plaintext risk-${data.impact}`}
+                    value={data.impact}
+                  />
+                </div>
+                <div className="col">
+                  <label className="form-label">Probability</label>
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.probability}
+                  />
+                </div>
+                <div className="col">
+                  <label className="form-label">Priority</label>
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.priority}
+                  />
+                </div>
+                <div className="col">
+                  <label className="form-label">Point</label>
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.impact * data.probability}
+                  />
+                </div>
               </div>
-              <div className="col">
-                <label class="form-label">Impact</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.impact}
-                />
-              </div>
-              <div className="col">
-                <label class="form-label">Probability</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.probability}
-                />
-              </div>
-              <div className="col">
-                <label class="form-label">Priority</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.priority}
-                />
-              </div>
-              <div className="col">
-                <label class="form-label">Point</label>
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.impact * data.probability}
-                />
-              </div>
-            </div>
-            <br></br>
+              <br />
 
-            <div class="mb-3 row">
-              <label className="col-sm-2 col-form-label">
-                Risk description
-              </label>
-              <div class="col-sm-10">
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.risk_description}
-                />
+              <div className="mb-3 row">
+                <label className="col-sm-2 col-form-label">
+                  Risk description
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.risk_description}
+                  />
+                </div>
               </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputPassword" className="col-sm-2 col-form-label">
-                Impact Description
-              </label>
-              <div class="col-sm-10">
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.impact_description}
-                />
+              <div className="mb-3 row">
+                <label className="col-sm-2 col-form-label">
+                  Impact Description
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.impact_description}
+                  />
+                </div>
               </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputPassword" className="col-sm-2 col-form-label">
-                Responsable plan
-              </label>
-              <div class="col-sm-10">
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.response_plan}
-                />
+              <div className="mb-3 row">
+                <label className="col-sm-2 col-form-label">
+                  Responsable plan
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.response_plan}
+                  />
+                </div>
               </div>
-            </div>
-            <div class="mb-3 row">
-              <label for="inputPassword" className="col-sm-2 col-form-label">
-                Owner
-              </label>
-              <div class="col-sm-10">
-                <input
-                  type="text"
-                  readonly
-                  class="form-control-plaintext"
-                  value={data.owner}
-                />
+              <div className="mb-3 row">
+                <label className="col-sm-2 col-form-label">Owner</label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    readOnly
+                    className="form-control-plaintext"
+                    value={data.owner}
+                  />
+                </div>
               </div>
-            </div>
-          </Form>
+            </Form>
+          </div>
         ))}
-        <a href="" style={{marginLeft:"90%", fontSize: "20px"}}>
-        Return{" "}
-        <FontAwesomeIcon icon={faRotateLeft}></FontAwesomeIcon>
+        <a href="" className="return-link">
+          Return{" "}
+          <FontAwesomeIcon icon={faRotateLeft}></FontAwesomeIcon>
         </a>
-       <br></br>
-       <br></br>
+        <br />
+        <br />
       </div>
     </div>
   );
--- src/routes/RoutesPlan.js
+++ src/routes/RoutesPlan.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {Login} from '../components/login/Login'
 import App from '../App';
 import { AddRisk } from '../components/riskRegister/AddRegister';
+import Matrix from '../components/Matrix/Matrix';
 
 export default function RoutesPlan() {
   return (
@@ -11,6 +12,7 @@ export default function RoutesPlan() {
             <Route path='/' element={<Login></Login>}></Route>
             <Route path='/app' element={<App></App>}></Route>
             <Route path='/addRisk' element={<AddRisk></AddRisk>}></Route>
+            <Route path='/matrix' element={<Matrix></Matrix>}></Route>
         </Routes>
     </Router>
   )
